cf534e039aa03d20e402b82a8d2b4f7565eafb93
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-04 19:13:26 +0900

    Jetpack Navigation Safe Args を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.compose)
     alias(libs.plugins.spotless)
     alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.androidx.navigation.safeargs)
 }
 
 android {
@@ -44,11 +45,6 @@ android {
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
     }
 }
 
