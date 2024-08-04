3630981d6271160af57d2305517c324099ecd846
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-04 23:15:18 +0900

    単体テストを全部作成

--- app/src/test/java/com/tfandkusu/ga913android/ui/list/LandmarkListViewModelTest.kt
+++ app/src/test/java/com/tfandkusu/ga913android/ui/list/LandmarkListViewModelTest.kt
@@ -3,12 +3,15 @@ package com.tfandkusu.ga913android.ui.list
 import com.tfandkusu.ga913android.data.LandmarkRepository
 import com.tfandkusu.ga913android.model.Landmark
 import com.tfandkusu.ga913android.presentation.MainDispatcherRule
+import com.tfandkusu.ga913android.ui.list.LandmarkListViewModel.Effect
 import com.tfandkusu.ga913android.ui.list.LandmarkListViewModel.Event
 import com.tfandkusu.ga913android.ui.list.LandmarkListViewModel.State
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
@@ -49,6 +52,58 @@ class LandmarkListViewModelTest {
             assertEquals(
                 State(
                     landmarks = landmarks,
+                ),
+                viewModel.state.value,
+            )
+        }
+
+    @Test
+    fun onClickLandmark() =
+        runTest {
+            viewModel.event(Event.OnClickLandmark(id = 2))
+            assertEquals(Effect.NavigateToLandmarkDetail(id = 2), viewModel.effect.first())
+        }
+
+    @Test
+    fun onChangeFavoritesOnly() =
+        runTest {
+            val landmarks =
+                listOf(
+                    mockk<Landmark> {
+                        every { id } returns 1
+                        every { isFavorite } returns false
+                    },
+                    mockk<Landmark> {
+                        every { id } returns 2
+                        every { isFavorite } returns true
+                    },
+                    mockk<Landmark> {
+                        every { id } returns 3
+                        every { isFavorite } returns false
+                    },
+                )
+            coEvery {
+                repository.list()
+            } returns
+                flowOf(
+                    landmarks,
+                )
+            viewModel.event(Event.Load)
+            assertEquals(
+                State(
+                    landmarks = landmarks,
+                    favoritesOnly = false,
+                ),
+                viewModel.state.value,
+            )
+            viewModel.event(Event.OnChangeFavoritesOnly(value = true))
+            assertEquals(
+                State(
+                    landmarks =
+                        listOf(
+                            landmarks[1],
+                        ),
+                    favoritesOnly = true,
                 ),
                 viewModel.state.value,
             )
