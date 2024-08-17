0e634d26d367baeeb3280462a57379f13305f860
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-17 20:22:52 +0900

    AnalyticsModule 作成

---
+++ app/src/main/java/com/tfandkusu/ga913android/analytics/AnalyticsModule.kt
@@ -0,0 +1,22 @@
+package com.tfandkusu.ga913android.analytics
+
+import android.content.Context
+import com.google.firebase.analytics.FirebaseAnalytics
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AnalyticsModule {
+    @Provides
+    @Singleton
+    fun provideFirebaseAnalytics(
+        @ApplicationContext context: Context,
+    ): FirebaseAnalytics {
+        return FirebaseAnalytics.getInstance(context)
+    }
+}
