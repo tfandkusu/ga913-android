4bcf46ace17632f58b4df06a4c09bb9684e65849
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-18 22:09:35 +0900

    detekt を導入した

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,19 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.detekt.gradle.plugin)
+}
+
+subprojects {
+    afterEvaluate {
+        // detekt の設定方法はこちらを参考にした。
+        // https://medium.com/@emrekoc/detekt-static-code-analyzer-for-android-c4c9bfd42a8c
+        plugins.apply(libs.plugins.detekt.gradle.plugin.get().pluginId)
+        detekt {
+            config.setFrom(file("$rootDir/config/detekt/detekt.yml"))
+            buildUponDefaultConfig = true
+            // 警告があってもエラーにしない
+            ignoreFailures = true
+        }
+    }
 }
