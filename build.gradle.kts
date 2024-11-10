cb40164d56fcd0622b9dba1d092e16c25b8b13e1
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-11-10 17:34:18 +0900

    AndroidLibraryComposeConventionPlugin 作成

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.compose) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.detekt.gradle.plugin)
     alias(libs.plugins.android.library) apply false
