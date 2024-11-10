3e137ebab67f4cdf3b6dc3590a1fad74564877cf
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-11-10 17:54:58 +0900

    feature:landmark モジュール作成

---
+++ feature/landmark/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.ga913android.android.library.compose)
+    alias(libs.plugins.ga913android.hilt)
+}
+
+android {
+    namespace = "com.tfandkusu.ga913android.landmark"
+}
+
+dependencies {
+    implementation(libs.ga913.kmp)
+}
